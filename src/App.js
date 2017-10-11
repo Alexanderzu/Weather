9bd552e1e4957c06dab6700e06c3690d7cab11b0
alexanderzu <artminor@yandex.ru>
2017-10-11 09:08:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const PLACES = [
+    { name: "Palo Alto", zip: "94303" },
+    { name: "San Jose", zip: "94088" },
+    { name: "Santa Cruz", zip: "95062" },
+    { name: "Honolulu", zip: "96803" }
+];
+
+
+
+class WeatherDisplay extends  Component {
+    render() {
+        return (
+            <h1> Hello! {this.props.zip} </h1>
+        )
+    }
+}
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            activePlace: 0,
+        };
+    }
+  render() {
+      const activePlace = this.state.activePlace;
+      return (
+      <div className="App">
+          <WeatherDisplay
+              key={activePlace}
+              zip={PLACES[activePlace].zip}
+          />
+          {
+              PLACES.map((place, index) => (
+                  <button
+                    key={index}
+                    onClick={() => {
+                        this.setState({ activePlace: index });
+                    }}
+                  >
+                      {place.name}
+                  </button>
+              ))
+          }
+      </div>
+    );
+  }
+}
+
+export default App;
